2288a1354f2d634704f3093dd5923be259e581a8
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-04-10 13:21:02 +0900

    change error handling

--- src/interface_kit_node.cpp
+++ src/interface_kit_node.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 
+#include <ros/duration.h>
 #include <ros/init.h>
 #include <ros/node_handle.h>
 
@@ -9,12 +10,14 @@ int main(int argc, char *argv[]) {
   ros::init(argc, argv, "phidget_interface_kit_node");
   ros::NodeHandle nh;
 
-  try {
-    phidget::InterfaceKit device(nh);
-    ros::spin();
-  } catch (const std::runtime_error &error) {
-    ROS_ERROR_STREAM(error.what());
-    return 1;
+  while (ros::ok()) {
+    try {
+      phidget::InterfaceKit device(nh);
+      ros::spin();
+    } catch (const std::runtime_error &error) {
+      ROS_ERROR_STREAM(error.what());
+      ros::Duration(1.).sleep();
+    }
   }
 
   return 0;
